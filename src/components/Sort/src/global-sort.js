b7d3fc5203a808746e2c0eaa0f6f0b6c0f2f64b1
= <=>
2019-11-21 14:49:21 +0530

    global sort working

---
+++ src/components/Sort/src/global-sort.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class GlobalSort {
+    constructor(sortOptions) {
+        this.options = sortOptions;
+    }
+    get() {
+        return this.options;
+    }
+}
+exports.default = GlobalSort;
+//# sourceMappingURL=global-sort.js.map
