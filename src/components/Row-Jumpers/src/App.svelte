acfa2ecfc72df6285ecab41b5ef760c6dcceed7c
Rahul Das <rahuldas@LPMB-Kol-Priyanjit.local>
2019-11-21 15:03:05 +0530

    develop row jumpers initial commmit

--- src/components/Row-Jumpers/src/App.svelte
+++ src/components/Row-Jumpers/src/App.svelte
@@ -1,8 +1,11 @@
 <script>
-	import Conditional from './Conditional.svelte'; 
-	function customHandler (e){
-		console.log(e.detail.text + 'Rahul');
-	}
+  import Input from './Input.svelte'; 
+
+  export let totalRows;
+  export let startRow;
+  export let endRow;
+
 </script>
-<h1>Test</h1>
-<svelte:component this={Conditional} on:customEvent={customHandler}/>
+
+<Input display={startRow || 1} type="start"/>
+<Input display={endRow || totalRows} type="end"/>
---
+++ src/components/Row-Jumpers/src/Input.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let display;
+    export let type;
+    import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+    // submit handler
+	function handleSubmit (e){
+		let keyCode = e.keyCode;
+        if (keyCode === 13){ // detect enter key
+			dispatch('rowSelected', { // fire component event
+                value: e.target.value,
+                type,
+				oriEvent: e
+			});
+		}  
+	}
+</script>
+
+<style>
+    input{
+        width: 50px;
+        height: 20px;
+    }
+</style>
+
+<input type="text" value={display} on:keydown={handleSubmit}>
