ccaf738e119f53e13530745fcb8db68358607e50
Rahul Das <rahuldas@LPMB-Kol-Priyanjit.local>
2019-11-21 17:45:34 +0530

    Grid component initial commit

--- src/components/Grid/src/App.svelte
+++ src/components/Grid/src/App.svelte
@@ -1,8 +1,3 @@
 <script>
-	import Conditional from './Conditional.svelte'; 
-	function customHandler (e){
-		console.log(e.detail.text + 'Rahul');
-	}
 </script>
 <h1>Test</h1>
-<svelte:component this={Conditional} on:customEvent={customHandler}/>
