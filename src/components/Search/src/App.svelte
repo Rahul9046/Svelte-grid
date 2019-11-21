a75c4e81408012fe04a427e7d02e62579a4987d6
Rahul Das <rahuldas@LPMB-Kol-Priyanjit.local>
2019-11-21 11:48:15 +0530

    Svelte component initial development

--- src/components/Search/src/App.svelte
+++ src/components/Search/src/App.svelte
@@ -1,8 +1,29 @@
 <script>
-	import Conditional from './Conditional.svelte'; 
-	function customHandler (e){
-		console.log(e.detail.text + 'Rahul');
+	export let searchBy;
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+    // submit handler
+	function handleSubmit (e){
+		let keyCode = e.keyCode;
+		if (keyCode === 13){ // detect enter key
+			dispatch('searchApplied', { // fire component event
+				value: e.target.value,
+				oriEvent: e
+			});
+		}  
 	}
 </script>
-<h1>Test</h1>
-<svelte:component this={Conditional} on:customEvent={customHandler}/>
+
+<style>
+	input{
+		width: 150px;
+		height: 25px;
+		border: 1px solid #cccccc;
+		border-radius: 3px;
+		border-style: solid;
+	}
+</style>
+
+<input type="text" placeholder="Search By.." value={searchBy || ''} on:keydown={handleSubmit}>
