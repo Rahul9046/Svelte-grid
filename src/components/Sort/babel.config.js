0f02d02094134eb9bdda4a0e3219ef5c5ac164c7
= <=>
2019-11-24 21:58:18 +0530

    adding puppeteer support

--- src/components/Sort/babel.config.js
+++ src/components/Sort/babel.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   presets: [
     [
+      "env",
       ['@babel/preset-env', {targets: {node: 'current'}}],
       '@babel/preset-typescript',
       {
--- src/components/Sort/tests/sort.test.ts
+++ src/components/Sort/tests/sort.test.ts
@@ -1,3 +1,41 @@
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4);
+var puppeteer = require('puppeteer');
+let browser,
+  page;
+const width = 1000,
+  height = 1000,
+  appSource: string = "/Users/dipbhattacharjee/Documents/officeDocs_fusionCharts/officeWork/FusionGrid/POCs/Svelte-grid/src/components/Sort/public/index.html";
+
+// test('two plus two is four', () => {
+//   expect(str + str).toBe(44);
+// });
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: [`--window-size=${width},${height}`]
+  });
+  page = await browser.newPage();
+  await page.goto('file://'+appSource);
 });
+afterAll(() => {
+  browser.close();
+});
+
+describe("Sort testing", () => {
+  test(
+    "You can choose any option from dropdown",
+    async (done) => {
+      page.waitForSelector('select').then(async ()=>{
+        page.select('select','Hi');
+        setTimeout(async ()=>{
+          let selectedOption = await page.$eval('select', selectEle => {return selectEle.value});
+          expect(selectedOption).toBe('Hi');
+          done();
+        }, 1000);
+      });
+    },
+    16000 // <<< Jasmine timeout
+  );
+});
+
